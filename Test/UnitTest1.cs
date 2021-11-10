c87f805ad6513fb672be5bf92edee96a12019aca
Vivek Gujari <vivekgujari@gmail.com>
2021-11-09 23:23:30 -0700

    commit

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -6,9 +6,18 @@ namespace Test
     public class Tests
     {
         [Test]
+        [TestCase(1, 2)]
         public static void Add_Tests(int a, int b)
-        { 
-            
+        {
+            int result = API.add(a, b);
+            Assert.That(result, Is.EqualTo(a + b));
+        }
+        [Test]
+        [TestCase(4, 2)]
+        public static void Div_Test(int a, int b)
+        {
+            int result = API.div(a, b);
+            Assert.That(result, Is.EqualTo(a / b));
         }
     }
 }
