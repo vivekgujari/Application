87bbeaf95aaf9c0758486eb94e1736f750fc3f89
Vivek Gujari <vivekgujari@gmail.com>
2021-11-09 21:02:16 -0700

    added API file

---
+++ Application/API.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public static class API
+{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int div(int a, int b)
+    {
+        if (b == 0)
+            throw new Exception("Cannot divide by zero");
+        return a / b;
+    }
+}
