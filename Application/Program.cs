cbe3cfe9f76728b97fefc6e4dac8878e54912e84
Vivek Gujari <vivekgujari@gmail.com>
2021-11-09 21:15:08 -0700

    changed something in main method

--- Application/Program.cs
+++ Application/Program.cs
@@ -10,7 +10,7 @@ namespace Application
             int a = Int32.Parse(Console.ReadLine());
             int b = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Addition of {0} and {1} is {2}", a, b, API.add(a, b));
-            Console.WriteLine("End of main program");
+            
         }
     }
 }
