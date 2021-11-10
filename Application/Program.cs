6efe6dae0d49509700cdfcee270098243bc16bcb
Vivek Gujari <vivekgujari@gmail.com>
2021-11-09 21:36:16 -0700

    added tests project

--- Application/Program.cs
+++ Application/Program.cs
@@ -9,8 +9,7 @@ namespace Application
             Console.WriteLine("Input two values");
             int a = Int32.Parse(Console.ReadLine());
             int b = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Addition of {0} and {1} is {2}", a, b, API.add(a, b));
-            
+            Console.WriteLine("Addition of {0} and {1} is {2}", a, b, API.add(a, b));           
         }
     }
 }
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public static void Add_Tests(int a, int b)
+        { 
+            
+        }
+    }
+}
